00d9fb25c724d417ebf1bbd5ee90fb016378b178
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-07 16:18:08 -0700

    finish concat

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
+
+
 """
 add module
 """
 
+
 def add(a: float, b: float) -> float:
     """return addintion"""
     return a + b
+
+
 
 if __name__ == '__main__':
     main()
