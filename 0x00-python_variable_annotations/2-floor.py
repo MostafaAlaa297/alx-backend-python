ad396074e05e2538a9093f9824ca9c36b8693183
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-07 16:36:28 -0700

    finish floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import math
+"""
+floor module
+"""
+
+
+def floor(n: float) -> int:
+    """return floor"""
+    return math.floor(n)
+
+
+if __name__ == '__main__':
+    main()
