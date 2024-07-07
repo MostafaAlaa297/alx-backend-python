1e27f024851dd9ebe59776012c9b008969dddc6b
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-07 16:48:17 -0700

    finish to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+to_string module
+"""
+
+
+def to_str(n: float) -> str:
+    """return string from number"""
+    return str(n)
+
+
+if __name__ == '__main__':
+    main()
