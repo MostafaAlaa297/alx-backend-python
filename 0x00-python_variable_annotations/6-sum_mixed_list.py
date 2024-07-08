4e3bb02b2b55b85a54e01dcf95197480ab12b209
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-08 05:20:34 -0700

    finish sum_mixed_list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+sum_mixed_list module
+"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[float]) -> float:
+    """return the sum of a mixed list"""
+    res = 0
+    for num in mxd_lst:
+        res += num
+    return res
