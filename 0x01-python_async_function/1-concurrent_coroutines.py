18224a7ebf2fde635e574c2e13443a024de78988
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-08 17:15:24 -0700

    sorted tasks

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,4 +12,4 @@ async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """returns the list of all the delays"""
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
-    return delays
+    return sorted(delays)
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -12,4 +12,4 @@ async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """returns the list of all the delays"""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
-    return delays
+    return sorted(delays)
