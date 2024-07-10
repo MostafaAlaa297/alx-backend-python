0e7a7854c75cb650278638d163fa758c4122389b
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-09 17:58:35 -0700

    fix: async_generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,9 +5,10 @@ async_generator module
 """
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator() -> float:
+async def async_generator() -> AsyncGenerator[float, None]:
     """Get rand int from yield after 1 second for 10 times"""
     for i in range(10):
         await asyncio.sleep(1)
