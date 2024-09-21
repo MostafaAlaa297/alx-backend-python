84d98202c4eabd76cb18c2208d26efda1ab7d7dd
MostafaAlaa297 <elaskary609@gmail.com>
2024-09-21 08:19:41 -0700

    fix: edit typos

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,19 +3,18 @@
 test_client
 """
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 from parameterized import parameterized
-from client import GithubOrgClient
-
+from client import GithubOrgClient  # Correct the import syntax
 
 class TestGithubOrgClient(unittest.TestCase):
-        """test using parameterized"""
+    """Test GithubOrgClient"""
+
     @parameterized.expand([
         ("google", {"login": "google", "id": 1}),
         ("abc", {"login": "abc", "id": 2}),
     ])
-
-    @patch('client.get_json', autospec=True)
+    @patch('client.get_json', autospec=True)  # This should be applied after parameterized
     def test_org(self, org_name, expected_response, mock_get_json):
         """Test that GithubOrgClient.org returns the correct value"""
         mock_get_json.return_value = expected_response
@@ -25,3 +24,6 @@ class TestGithubOrgClient(unittest.TestCase):
 
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(response, expected_response)
+
+if __name__ == '__main__':
+    unittest.main()
