02d3ccffc181bc983685585726060f778ecd89e2
MostafaAlaa297 <elaskary609@gmail.com>
2024-09-22 08:04:19 -0700

    More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -30,11 +30,32 @@ class TestGithubOrgClient(unittest.TestCase):
         expected_repos_url = "https://api.github.com/orgs/google/repos"
         with mock.patch(
                'GithubOrgClient._public_repos_url', new_callable=PropertyMock) as mock_org:
-            mock_org.return_value = { "repos_url": expected_repo_url }
+            mock_org.return_value = { "repos_url": expected_repos_url }
 
             client = GithubOrgClient("google")
 
             self.assertEqual(client._public_repos_url, expected_repos_url)
+
+
+    @patch('client.get_json', autospec=True)
+    def test_public_repos(self):
+        expected_repos = ["repo1", "repo2"]
+        expected_repos_url = "https://api.github.com/orgs/google/repos"
+
+        with mock.patch(
+                'GithubOrgClient._public_repos_url', new_callable=PropertyMock) as mock_repo_url:
+            mock_repo_url.return_value = expected_repos_url
+            mock_get_json.return_value = expeted_repos
+            
+            client = GithubOrgClient("google")
+
+            repos = client.public_repos()
+
+            self.assertEqual(repos, expected_repos)
+
+            mock_repo_url.assert_called_once()
+
+            mock_get_json.assert_called_once_with(expected_repos_url)
 
 
 if __name__ == '__main__':
